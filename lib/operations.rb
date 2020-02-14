8ef3df2ec783de61d30b70854062a31386730959
kyle molidor <k_molidor@yahoo.com>
2020-02-14 05:40:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40
+    return "That is unsafe"
+  elsif speed > 60
+    return "That is unsafe"
+  else
+    return "That is safe"
 end
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 ? "unsafe" : "safe"
+  speed > 60 ? "unsafe" : "safe"
 end
-	
 
 
